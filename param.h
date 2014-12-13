b926d97eb754caca9b69cbffeb3e4d3dfcb97aa0
YuuShigetani <s2g4t1n2@gmail.com>
2014-12-13 22:36:05 +0900

    init

---
+++ param.h
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+#define MX0     33
+#define MY0     33
+#define MZ0     65
+#define MIMAX     19
+#define MJMAX     19
+#define MKMAX     35
+#define NDX0      2
+#define NDY0      2
+#define NDZ0      2
